05122673227a7c4bd0d55fc8064060bdd77e4691
Walkyria <tonolli.wal@gmail.com>
2025-07-15 20:17:07 -0300

    add listener para fechar menu se tocado fora dele.

--- javascript/main.js
+++ javascript/main.js
@@ -3,3 +3,15 @@ function menuOnClick() {
   document.getElementById("nav").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
+document.addEventListener('click', function(event) {
+  const menu = document.getElementById("menu-bar");
+  const targetElement = event.target;
+
+
+  if (!menu.contains(targetElement)) {
+    menuOnClick()
+    menu.classList.remove('ativo');
+    console.log('Menu fechado!');
+  }
+});
